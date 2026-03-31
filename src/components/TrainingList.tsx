81f68b7dcad7f9a169563478d8332f644f78e901
HaniNghi <vmmn27@gmail.com>
2026-03-31 21:13:05 +0300

    fix(training list): fix not shown training list and format date time

--- src/components/TrainingList.tsx
+++ src/components/TrainingList.tsx
@@ -3,19 +3,27 @@ import type { TrainingData } from "../types";
 import type { GridColDef} from "@mui/x-data-grid";
 import { DataGrid } from "@mui/x-data-grid";
 import { fetchTraining } from "../api";
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
 
-export default function CustomerList() {
+
+
+export default function TrainingList() {
   const [trainings, setTrainings] = useState<TrainingData[]>([]);
+  dayjs.extend(utc);
+
 
   const columns: GridColDef[] = [
-    { field: "date", headerName: "Date", width: 150 },
-    { field: "duration", headerName: "Duration", width: 150 },
+    { field: "date", headerName: "Date", width: 250, valueFormatter: (params) =>
+      dayjs.utc(params).format('DD/MM/YYYY, HH:mm'),
+  },
+    { field: "duration", headerName: "Duration (minutes)", width: 150 },
     { field: "activity", headerName: "Activity", width: 150 },
   ];
 
   const getTrainings = () => {
     fetchTraining()
-      .then((data) => setTrainings(data._embedded.customers))
+      .then((data) => setTrainings(data._embedded.trainings))
       .catch((err) => console.error(err));
   };
 
