c87cfaca2be1a07349e5fd2ab0063a80730a5d71
HaniNghi <vmmn27@gmail.com>
2026-04-22 16:11:57 +0300

    add: create new line chart to display total training per day

--- src/pages/Chart.tsx
+++ src/pages/Chart.tsx
@@ -1,8 +1,10 @@
 import { BarChart } from "@mui/x-charts/BarChart";
+import { LineChart } from "@mui/x-charts/LineChart";
 import { useEffect, useState } from "react";
 import { fetchTraining } from "../api";
 import type { Training } from "../types";
 import { groupBy, sumBy } from "lodash";
+import dayjs from "dayjs";
 
 export default function Chart() {
   const [trainings, setTrainings] = useState<Training[]>([]);
@@ -12,6 +14,16 @@ export default function Chart() {
       .then((data) => setTrainings(data))
       .catch((err) => console.error(err));
   };
+
+  const byDate = Object.entries(
+    groupBy(trainings, (t) => dayjs(t.date).format("YYYY-MM-DD")),
+  ).map(([date, group]) => ({
+    date,
+    minutes: sumBy(group, "duration"),
+  }));
+
+  // sort dates properly
+  byDate.sort((a, b) => dayjs(a.date).valueOf() - dayjs(b.date).valueOf());
 
   useEffect(() => {
     getTrainings();
@@ -24,12 +36,32 @@ export default function Chart() {
     }),
   );
   return (
-    <BarChart
-      xAxis={[
-        { scaleType: "band", data: activityStats.map((a) => a.activity) },
-      ]}
-      series={[{ data: activityStats.map((a) => a.minutes) }]}
-      height={600}
-    />
+    <>
+      <BarChart
+        xAxis={[
+          { scaleType: "band", data: activityStats.map((a) => a.activity) },
+        ]}
+        series={[{ data: activityStats.map((a) => a.minutes) }]}
+        height={400}
+        width={800}
+      />
+      <LineChart
+        xAxis={[
+          {
+            scaleType: "point",
+            data: byDate.map(
+              (d) => dayjs(d.date).format("DD MMM"), // nicer display
+            ),
+          },
+        ]}
+        series={[
+          {
+            data: byDate.map((d) => d.minutes),
+          },
+        ]}
+        height={400}
+        width={800}
+      />
+    </>
   );
 }
